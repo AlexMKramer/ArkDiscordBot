e1d6e68653d76798212fea216c79ba8501f92fd0
AlexMKramer <93092632+AlexMKramer@users.noreply.github.com>
2023-11-06 10:13:33 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# ArkDiscordBot
+## A Discord bot for your Ark Survival Ascended/Evolved server.
+
+This is my very basic attempt at making a discord bot for controlling an Ark server using RCON. It's written in Python and uses the py-cord library.
+I am not a programmer by any means, so this is probably not the best code you'll ever see. I'm just doing this for fun.
+Any suggestions or help is welcome.
---
+++ main.py
@@ -0,0 +1,53 @@
+import discord
+from discord.ext import commands
+from rcon.source import rcon
+import docker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+SERVER_IP = os.getenv('SERVER_IP')
+SERVER_PORT = int(os.getenv('SERVER_PORT'))
+RCON_PASSWORD = os.getenv('RCON_PASSWORD')
+TOKEN = os.getenv('BOT_TOKEN')
+# Bot setup
+bot = commands.Bot(command_prefix="!")
+
+# Docker client setup
+docker_client = docker.from_env()
+
+@bot.command()
+async def check_status(ctx):
+    try:
+        response = await rcon(
+            'admincheat', 'listplayers',
+            host=SERVER_IP, port=SERVER_PORT, passwd=RCON_PASSWORD)
+        await ctx.send(f'Server status: {response}')
+    except Exception as e:
+        await ctx.send(f'Error checking server status: {e}')
+
+
+@bot.command()
+async def start_server(ctx):
+    # Check if server is running
+    container = docker_client.containers.get('ark-server')
+    if container.status == 'running':
+        await ctx.send('Server is already running')
+        return
+    else:
+        docker_client.containers.run('ark-server', detach=True)
+        await ctx.send('Starting ARK server...')
+
+@bot.command()
+async def stop_server(ctx):
+    container = docker_client.containers.get('ark-server')
+    # Check if server is running
+    if container.status != 'running':
+        await ctx.send('Server is not running')
+        return
+    else:
+        container.stop()
+        await ctx.send('Stopping ARK server...')
+
+# Run the bot
+bot.run(TOKEN)
